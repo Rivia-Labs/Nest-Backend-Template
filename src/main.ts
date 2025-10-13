749a430435949506a47be33435873b9f9ff25ccb
igorabreu29 <abreusense@gmail.com>
2025-10-13 20:55:01 -0300

    style: lint

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,5 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
-import { UserEntity } from "./domain/accounts/enterprise/entities/user-entity";
-import { Email } from "./domain/accounts/enterprise/entities/value-object/email-vo";
-import { UserStatus } from "./domain/accounts/enterprise/entities/value-object/user-status-vo";
 import { EnvService } from "./infra/configs/env/env.service";
 
 async function bootstrap() {
